b9682b6902ff5dad36fa4cceb59416de848cd114
Inferno339 <iyferzv@gmail.com>
2024-10-24 15:59:07 +0300

    длина отрезка

--- ConsoleAppgit/Program.cs
+++ ConsoleAppgit/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main()
     {
-        // Генерация двух случайных точек на плоскости
+        // Генерация двух  точек 
         Random random = new Random();
 
         int x1 = random.Next(100);
         int y1 = random.Next(100);
         int x2 = random.Next(100);
         int y2 = random.Next(100);
-        //две точки
+        
         double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         double dlinaokr = distance * 2;
         Console.WriteLine($"Точка 1: ({x1}, {y1})");
