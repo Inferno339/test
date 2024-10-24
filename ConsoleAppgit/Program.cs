a9b0b9807d0387383900322f2d9d27ece26f0f60
Inferno339 <iyferzv@gmail.com>
2024-10-24 16:12:28 +0300

    площадь круга

--- ConsoleAppgit/Program.cs
+++ ConsoleAppgit/Program.cs
@@ -18,7 +18,6 @@ class Program
         Console.WriteLine($"Точка 2: ({x2}, {y2})");
         Console.WriteLine($"Расстояние между точками: {distance}");
 
-        // Ожидание нажатия клавиши
         Console.ReadKey();
     }
 }
--- ConsoleAppgit/obj/Debug/net8.0/ConsoleAppgit.AssemblyInfo.cs
+++ ConsoleAppgit/obj/Debug/net8.0/ConsoleAppgit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppgit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c443d310a489fcdafee8f9bcf19d96692cf5f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppgit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppgit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
