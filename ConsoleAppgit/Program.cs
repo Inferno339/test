94fb14398021cc35f10653f10307a431a9cbd9f7
Inferno339 <iyferzv@gmail.com>
2024-10-24 15:57:14 +0300

    координаты точек

---
+++ ConsoleAppgit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Генерация двух случайных точек на плоскости
+        Random random = new Random();
+
+        int x1 = random.Next(100);
+        int y1 = random.Next(100);
+        int x2 = random.Next(100);
+        int y2 = random.Next(100);
+        //две точки
+        double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        double dlinaokr = distance * 2;
+        Console.WriteLine($"Точка 1: ({x1}, {y1})");
+        Console.WriteLine($"Точка 2: ({x2}, {y2})");
+        Console.WriteLine($"Расстояние между точками: {distance}");
+
+        // Ожидание нажатия клавиши для выхода
+        Console.ReadKey();
+    }
+}
